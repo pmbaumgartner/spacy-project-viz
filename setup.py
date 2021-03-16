3b5e48e9955ad6ceeacb4b0282b25fe99f65dc7d
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2021-03-16 10:47:59 -0400

    clean up setup

--- setup.py
+++ setup.py
@@ -1,60 +1,13 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+from setuptools import find_packages, setup
 
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
-    name="spacy-project-viz",  # Required
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1",  # Required
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
-    description="CLI Tool to help generate a mermaid.js or graphviz chart of commands, dependencies, outputs, and optionally variables from a spaCy project.yml file.",  # Optional
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url="https://github.com/pmbaumgartner/spacy-project-viz",  # Optional
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author="Peter Baumgartner",  # Optional
-    # This should be a valid email address corresponding to the author listed
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={"": "viz"},  # Optional
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(),  # Required
+    name="spacy-project-viz",
+    version="0.0.1",
+    description="CLI Tool to help generate a mermaid.js or graphviz chart of "
+    "commands, dependencies, outputs, and optionally variables from a spaCy project.yml file.",
+    url="https://github.com/pmbaumgartner/spacy-project-viz",
+    author="Peter Baumgartner",
+    packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=["spacy>=3.0.0", "srsly>=2.4.0", "typer>=0.3.2"],
     extras_require={
@@ -62,14 +15,7 @@ setup(
         "graphviz": ["graphviz>=0.16"],
         "all": ["pytest", "pytest-cov", "graphviz>=0.16"],
     },
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
+    entry_points={
         "console_scripts": [
             "spacy-project-viz=spacy_project_viz.cli:main",
         ],
